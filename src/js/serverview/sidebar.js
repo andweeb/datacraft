02b3d286261c3cadac301990c1434210c0ecfe5f
Andrew Kwon <askwon@ucdavis.edu>
2016-01-26 11:56:32 -0800

    Replaced list with a table to show logged player time

--- src/js/serverview/sidebar.js
+++ src/js/serverview/sidebar.js
@@ -1,3 +1,22 @@
+function convertPlaytime(seconds) {
+	var value = seconds;
+	var units = {
+		 "day": 24*60*60,
+		 "hr": 60*60,
+		 "min": 60,
+		 "sec": 1
+	}
+
+	var result = []
+	for(var name in units) {
+	  var p =  Math.floor(value/units[name]);
+	  if(p == 1) result.push(p + " " + name);
+	  if(p >= 2) result.push(p + " " + name + "s");
+	  value %= units[name]
+	}
+	return result.join(', ');
+}
+
 // Server id starts from 1 (not 0)
 function initServerviewSidebar(id) {
     document.getElementsByClassName('title')[0].innerHTML = `Server ${id} Overview`;
@@ -19,76 +38,66 @@ function initServerviewSidebar(id) {
 }
 
 function initPlayerList(id) {
-    // d3.csv(`data/servers/serverview${id}.csv`, (error, data) => {
-    //     if(error) throw error;
-    //     console.log(data);
-    // });
-
-    var faces = [
-        'alex.png',
-        'creeper.jpg',
-        'ender.png',
-        'steve.png',
-        'villager.png',
-        'grayface.png',
-        'ironman.jpg',
-        'cavespidey.jpg',
-        'pigman.png',
-        'zombie.png',
-        'wolf.png',
-        'skelly.png',
-        'witherskelly.png',
-        'wither.png',
-        'pumpkin.png',
-        'sheep.png',
-        'mooshroom.png',
-        'golem.png',
-        'chicken.png',
-        'magma.jpg',
-        'pig.jpg',
-        'cow.png',
-        'blaze.png',
-        'slime.png',
-        'elderguardian.png',
-        'squid.png',
-        'witch.png',
-        'spider.png',
-        'guardian.png',
-        'ghast.png',
-        'enderdragon.png',
-        'endermite.png',
-        'bajan.png',
-        'kakashi.jpg',
-        'walrus.jpg',
-        'deadpool.png',
-        'bionicle.png',
-    ];
 
     document.getElementsByClassName('info')[0].innerHTML = 'Loading...';
 
-    var playerList = document.createElement('ul');
-    playerList.className = 'player-list';
-    document.getElementsByClassName('sidebar')[0].appendChild(playerList);
-    for(var i = 0; i < 100; i++) {
-        var player = document.createElement('li');
+    var playerTable = document.createElement('table');
+    playerTable.className = 'player-table';
 
-        var icon = document.createElement('img');
-        var face = faces[Math.round(Math.random() * (faces.length - 1) + 0)]
-        icon.src = `data/faces/${face}`;
-        icon.style.width = '1rem';
-        icon.style.paddingLeft = '.5rem';
-        icon.style.paddingRight = '.4rem';
-        player.appendChild(icon);
+    var headerRow = document.createElement('tr');
+    headerRow.className = 'player-header-row';
+    var headerName = document.createElement('th');
+    headerName.innerHTML = 'Player Name';
+    var headerTime = document.createElement('th');
+    headerTime.innerHTML = 'Logged Playtime';
+    headerTime.style.paddingLeft = '2.1rem';
 
-        var playerName = document.createElement('span');
-        playerName.innerHTML = getName(4, 8);
-        player.appendChild(playerName);
+    headerRow.appendChild(headerName);
+    headerRow.appendChild(headerTime);
+    playerTable.appendChild(headerRow);
 
-        playerList.appendChild(player);
-    }
+    d3.json(`data/servers/server${id}players.json`, (error, data) => {
+        if(error) throw error;
+
+        var count = 0;
+        for(var name in data) {
+
+            var player = document.createElement('tr');
+            player.className = 'player';
+            var nameBox = document.createElement('td');
+            nameBox.className = 'player-name';
+            var timeBox = document.createElement('td');
+            timeBox.className = 'playtime';
+            var icon = document.createElement('img');
+
+            // Create face icon and append to the table element 
+            var imgSrc = faces[Math.round(Math.random() * (faces.length - 1) + 0)];
+            icon.src = `data/faces/${imgSrc}`;
+            icon.style.width = '1rem';
+            icon.style.paddingLeft = '.5rem';
+            icon.style.paddingRight = '.4rem';
+            nameBox.appendChild(icon);
+            nameBox.innerHTML += getName(4, 8);
+
+            timeBox.innerHTML = convertPlaytime(data[name].playtime*60);
+
+            player.id = name;
+            player.className = 'player';
+            player.appendChild(nameBox);
+            player.appendChild(timeBox);
+            player.playtime = data[name].playtime;
+            player.actions = data[name].actions;
+
+            playerTable.appendChild(player);
+
+            count++;
+            if(count === 100) 
+                break;
+        }
+    });
 
     document.getElementsByClassName('info')[0].innerHTML = '';
-    document.getElementsByClassName('info')[0].appendChild(playerList);
+    document.getElementsByClassName('info')[0].appendChild(playerTable);
 
     servercraft(id);
 }
