0623f1989599e164ac191f4ac116b4bf691c95a3
Andrew Kwon <askwon@ucdavis.edu>
2016-01-26 11:57:03 -0800

    Temporarily commented out main viz

--- src/js/serverview/viz.js
+++ src/js/serverview/viz.js
@@ -1,6 +1,10 @@
+var playerData;
+
 function servercraft(id) {
-    d3.json(`data/servers/server${id}players.json`, (error, data) => {
-        if(error) throw error;
-        console.log(JSON.stringify(data,null,2));
-    });
+    
+    // d3.json(`data/servers/server${id}players.json`, (error, data) => {
+    //     if(error) throw error;
+    //     console.log(JSON.stringify(data,null,2));
+    // });
+
 }
