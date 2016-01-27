c2c3cefd89327b59deb9661eec77c3c78cf3d4da
Andrew Kwon <askwon@ucdavis.edu>
2016-01-27 14:43:05 -0800

    Added server id to display on a tooltip

--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -1,12 +1,16 @@
 var tooltip = d3.select('div.tooltip');
 
-function tooltipMouseover(html, x, y) {
+function tooltipMouseover(data, bubble, i) {
     // Load tooltip data here?
     var mouseCoords = d3.mouse(tooltip.node().parentElement);
+    var style = `"position: absolute;
+                 text-align: center;
+                 width: 100%;
+                 top: 0;
+                 left:0;"`;
 
-    tooltip.style('visibility', 'visible').html(html)
-        .style('top', x)
-        .style('left', y);
+    tooltip.style('visibility', 'visible')
+        .html(`<strong style=${style}>Server ${bubble.id}</strong>`);
 }
 
 function tooltipMousemove() {
