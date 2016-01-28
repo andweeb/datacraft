23afb40dce2a9fa681addb0eb6d7825876bb9364
Andrew Kwon <askwon@ucdavis.edu>
2016-01-27 21:09:35 -0800

    Implemented radar chart (dblclick listener)

--- src/js/serverview/viz.js
+++ src/js/serverview/viz.js
@@ -8,3 +8,49 @@ function servercraft(id) {
     // });
 
 }
+
+function onPlayerClick(data, name, element) {
+    console.log('--> in onPlayerClick()');
+    console.log(data);
+    console.log(element);
+    var translate = `translate(-${width/3}px, -${height/3}px)`;
+    hsl(0.10, 78%, 91%)
+
+    var meep = [
+      {
+        className: 'germany', // optional can be used for styling
+        axes: [
+          {axis: "Crafter", value: 0.13}, 
+          {axis: "PvPer", value: 0.6}, 
+          {axis: "Builder", value: 0.5},  
+          {axis: "Socializer", value: 0.9},  
+          {axis: "Miner", value: 0.2}
+        ]
+      },
+      {
+        className: 'argentina',
+        axes: [
+          {axis: "strength", value: 0.6}, 
+          {axis: "intelligence", value: 0.7}, 
+          {axis: "charisma", value: 0.10},  
+          {axis: "dexterity", value: 0.13},  
+          {axis: "luck", value: 0.9}
+        ]
+      }
+    ]
+
+    var chart = RadarChart.chart();
+
+    chart.config({ 
+        levels: 5,
+        maxValue: 1,
+        minValue: 0,
+    });
+
+    canvas.append('g')
+        .classed('focus', 1)
+        .datum(meep)
+        .transition()
+        .style('transform', translate)
+        .call(chart);
+}
